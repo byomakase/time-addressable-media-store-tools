74f8a4b476ddfd8a9f381f70c4143c0ce53b2658
Dan Johns <danjhd@amazon.com>
2025-02-13 16:52:47 +0000

    fix issue with rendering segment locale time when single timestamp used

--- frontend/src/utils/parseTimerange.js
+++ frontend/src/utils/parseTimerange.js
@@ -2,13 +2,20 @@ import { DATE_FORMAT } from "@/constants";
 import { DateTime } from "luxon";
 
 const parseTimerange = (timerange) => {
-    const [start, end] = timerange
+    const secondsValues = timerange
         .substring(1)
         .split("_")
-        .map((value) => DateTime.fromSeconds(parseInt(value.split(":")[0], 10)));
-    return {
-        start: start.toLocaleString(DATE_FORMAT),
-        end: end.toLocaleString(DATE_FORMAT),
+        .map((value) => parseInt(value.split(":")[0], 10));
+    if (secondsValues.length === 1) {
+        return {
+            start: DateTime.fromSeconds(secondsValues[0]).toLocaleString(DATE_FORMAT),
+            end: null,
+        };
+    } else if (secondsValues.length === 2) {
+        return {
+            start: DateTime.fromSeconds(secondsValues[0]).toLocaleString(DATE_FORMAT),
+            end: DateTime.fromSeconds(secondsValues[1]).toLocaleString(DATE_FORMAT),
+        };
     };
 };
 
