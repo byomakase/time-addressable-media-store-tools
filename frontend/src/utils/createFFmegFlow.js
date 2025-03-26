d1c94ae17070265c833c35982e3b833c81306c4c
Dan Johns <danjhd@amazon.com>
2025-03-26 09:15:27 +0000

    fix TAMS incorrect source creation for FFmpeg tasks

--- frontend/src/utils/createFFmegFlow.js
+++ frontend/src/utils/createFFmegFlow.js
@@ -1,10 +1,10 @@
 import { useApi } from "@/hooks/useApi";
 
-const applyChanges = (flow, changes) => {
+const applyChanges = (flow, changes, sourceId) => {
   const newFlow = {
     ...flow,
     id: crypto.randomUUID(),
-    source_id: crypto.randomUUID(),
+    source_id: sourceId,
   };
   Object.entries(changes).forEach(([key, value]) => {
     if (value === null) {
@@ -33,12 +33,40 @@ const applyChanges = (flow, changes) => {
 const createFFmegFlow = async (flowId, changes) => {
   const { get, put } = useApi();
   const flow = await get(`/flows/${flowId}`);
-  const newFlow = applyChanges(flow, changes);
+  let newSourceId = changes.format ? crypto.randomUUID() : flow.source_id;
+  const flowCollectionsMap = new Map();
+  if (flow.collected_by) {
+    await Promise.all(
+      flow.collected_by.map(async (collectedById) => {
+        const flowCollection = await get(
+          `/flows/${collectedById}/flow_collection`
+        );
+        flowCollectionsMap.set(collectedById, flowCollection);
+      })
+    );
+  }
+  if (flow.collected_by && changes.format) {
+    const formatSources = await Promise.all(
+      flow.collected_by.map(async (collectedById) => {
+        const flowCollection = flowCollectionsMap.get(collectedById);
+        return await Promise.all(
+          flowCollection.map(async ({ id }) => {
+            const collectedFlow = await get(`/flows/${id}`);
+            return collectedFlow.format === changes.format
+              ? collectedFlow.source_id
+              : null;
+          })
+        );
+      })
+    ).then((results) => results.flat());
+    newSourceId = formatSources.find((sourceId) => sourceId) ?? newSourceId;
+  }
+  const newFlow = applyChanges(flow, changes, newSourceId);
   delete newFlow.created_by;
   delete newFlow.updated_by;
   await put(`/flows/${newFlow.id}`, newFlow);
   flow.collected_by.forEach(async (collectedById) => {
-    const flowCollection = await get(`/flows/${collectedById}/flow_collection`);
+    const flowCollection = flowCollectionsMap.get(collectedById);
     await put(`/flows/${collectedById}/flow_collection`, [
       ...flowCollection,
       { id: newFlow.id, role: newFlow.format.split(":")[3] },
