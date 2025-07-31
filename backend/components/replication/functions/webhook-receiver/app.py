ad395396ef038f29a7da1e006ff3012b8707c45f
Dan Johns <danjhd@amazon.com>
2025-07-31 16:46:02 +0100

    webhooks receiver will forward all other received to the tams Bus with replication source

--- backend/components/replication/functions/webhook-receiver/app.py
+++ backend/components/replication/functions/webhook-receiver/app.py
@@ -1,5 +1,6 @@
 import os
 import json
+from datetime import datetime
 import boto3
 
 from aws_lambda_powertools import Logger, Tracer
@@ -14,18 +15,13 @@ logger = Logger()
 app = APIGatewayRestResolver(enable_validation=True)
 
 sqs = boto3.client("sqs")
+eb = boto3.client("events")
 queue_url = os.environ["QUEUE_URL"]
+event_bus_name = os.environ["EVENT_BUS_NAME"]
 
 
-@app.post("/<id>")
 @tracer.capture_method(capture_response=False)
-def process_webhook_event(
-    payload: Annotated[dict, Body()], _: Annotated[str, Path(alias="id")]
-):
-    if payload["event_type"] != "flows/segments_added":
-        # Only process segments_added events received
-        return
-    event = payload["event"]
+def segments_added(event):
     entries = []
     for segment in event["segments"]:
         if not segment.get("get_urls"):
@@ -53,7 +49,34 @@ def process_webhook_event(
         # Only send messages if messages exist
         return
     sqs.send_message_batch(QueueUrl=queue_url, Entries=entries)
-    return
+
+
+@tracer.capture_method(capture_response=False)
+def put_event(path_id, payload):
+    eb.put_events(
+        Entries=[
+            {
+                "EventBusName": event_bus_name,
+                "Time": datetime.fromisoformat(payload["event_timestamp"]),
+                "Source": "replication",
+                "DetailType": payload["event_type"],
+                "Detail": json.dumps(payload["event"]),
+                "Resources": [path_id],
+            },
+        ],
+    )
+
+
+@app.post("/<id>")
+@tracer.capture_method(capture_response=False)
+def process_webhook_event(
+    payload: Annotated[dict, Body()], path_id: Annotated[str, Path(alias="id")]
+):
+    match payload["event_type"]:
+        case "flows/segments_added":
+            segments_added(payload["event"])
+        case _:
+            put_event(path_id, payload)
 
 
 @logger.inject_lambda_context(
